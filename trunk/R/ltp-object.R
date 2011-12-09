a55a992fdba6fec1aec684edbfac9b0e4ba4dea0
matteo.redaelli <matteo.redaelli@82341774-bbb0-a707-fdb5-f16d5bdc7e83>
2011-12-09 17:30:02 +0000

    updated licence info at the beginning of all files

--- trunk/R/ltp-object.R
+++ trunk/R/ltp-object.R
@@ -1,6 +1,23 @@
-#==========================================================
-# CLASS DEFINITION *** CLASS DEFINITION *** CLASS DEFINITION
-#==========================================================
+## This program is fre esoftware: you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation, either version 3 of the License, or
+## any later version.
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+## project name: ltp
+## project website: http://code.google.com/p/ltp/
+## created: 2011
+
+##==========================================================
+## CLASS DEFINITION *** CLASS DEFINITION *** CLASS DEFINITION
+##==========================================================
 
 #setClassUnion("matrixOrNULL", c("matrix", "NULL"))
 setClassUnion("data.frameOrNULL", c("data.frame", "NULL"))
